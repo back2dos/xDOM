5e71352ac1bb6dabb19467e0849e43cd6082ff4c
Juraj Kirchheim <juraj.kirchheim@doclerholding.com>
2019-11-02 08:04:09 +0100

    Remove pick.first

--- src/xdom/XDom.hx
+++ src/xdom/XDom.hx
@@ -2,10 +2,14 @@ package xdom;
 
 import js.html.*;
 
+#if !haxe4
+  typedef ConsoleInstance = Console;
+#end
+
 class XDom {
   static public var window(default, never):Window = js.Browser.window;
   static public var document(default, never):xdom.html.Document = js.Browser.document;
-  static public var console(default, never):pick.First<"ConsoleInstance", "Console"> = js.Browser.console;
+  static public var console(default, never):ConsoleInstance = js.Browser.console;
 
   static public inline function alert(v:Dynamic) js.Browser.alert(v);
 
